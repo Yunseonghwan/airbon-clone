2ca9f36eef4a126bbecf6e566ef1a3d3e6efe74b
Yunseonghwan <sunghwan0208@naver.com>
2020-10-20 01:00:07 +0900

    commit

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models  # 같은 위치에있는 models를 불러옴
+
 
 # Register your models here.
+
+
+@admin.register(models.User)  # 데코레이터  models.User위에 CustomUserAdmin적용시키겟다는의미
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser  # 사용자인증
 from django.db import models
 
 # Create your models here.
@@ -6,4 +6,35 @@ from django.db import models
 
 class User(AbstractUser):
 
-    pass
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(
+        max_length=10, choices=GENDER_CHOICES, blank=True
+    )  # CharField는 하나의 agrument 가 필요, 싱글라인 text 글자수 제한 필요(커스텀가능)
+    bio = models.TextField(default="", blank=True)
+    # null은 데이터베이스 값이고 실제로 빈값 허용해줄려면 blank 사용
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
